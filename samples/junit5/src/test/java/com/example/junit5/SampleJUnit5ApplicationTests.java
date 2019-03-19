13668ed4aa02c9230be74ec5046b22b6e9d35577
Liang Zhou <lzhoucs@gmail.com>
2019-03-19 09:23:07 -0500

    Create nested test for demo. Clean up imports.

--- samples/junit5/src/test/java/com/example/junit5/SampleJUnit5ApplicationTests.java
+++ samples/junit5/src/test/java/com/example/junit5/SampleJUnit5ApplicationTests.java
@@ -16,24 +16,17 @@
 
 package com.example.junit5;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -45,9 +38,19 @@ public class SampleJUnit5ApplicationTests {
 
 	@Test
 	public void sample() throws Exception {
-		this.mockMvc.perform(get("/hello"))
+		mockMvc.perform(get("/hello"))
 				.andExpect(status().isOk())
 				.andDo(document("sample"));
+	}
+
+	@Nested
+	class NestedTests {
+		@Test
+		public void sampleNested() throws Exception {
+			mockMvc.perform(get("/hello"))
+				.andExpect(status().isOk())
+				.andDo(document("sample"));
+		}
 	}
 
 }
